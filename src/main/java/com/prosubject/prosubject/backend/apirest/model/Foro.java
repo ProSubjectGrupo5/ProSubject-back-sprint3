8949234b9aa030ce2e570d7deca40a5609b86b92
unknown <albertoemerita@gmail.com>
2020-03-20 13:35:40 +0100

    Entidad Foro

--- src/main/java/com/prosubject/prosubject/backend/apirest/model/Foro.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/model/Foro.java
@@ -1,51 +1,73 @@
 package com.prosubject.prosubject.backend.apirest.model;
 
-import java.io.Serializable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Entity(name = "foros")
-public class Foro implements Serializable{
+public class Foro {
 
-	
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(nullable = false)
-	@NotBlank
-	private String titulo;
-	
+    @Id
+    @GeneratedValue
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    @Column(nullable = false, length = 32)
+    private String titulo;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @Column(nullable = false, length = 1024)
+    private String contenido;
+    
+    @Column
+    private Date fechaCreacion;
 
-	public String getTitulo() {
-		return titulo;
-	}
+    @OneToMany
+    private List<Respuesta> respuestas;
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	
-	
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return titulo;
+    }
+
+    public void setTitle(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContent() {
+        return contenido;
+    }
+
+    public void setContent(String content) {
+        this.contenido = content;
+    }
+
+    public List<Respuesta> getRespuestas() {
+        return respuestas;
+    }
+
+    public void setRespuestas(List<Respuesta> respuestas) {
+        this.respuestas = respuestas;
+    }
 
 
-	
 }
