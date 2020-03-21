a52cea7e475a7af30893f3f611f12f37e3798c8c
anton <antonioromerocaceres@hotmail.com>
2020-03-21 13:20:11 +0100

    CrossOrigin Respuesta

--- src/main/java/com/prosubject/prosubject/backend/apirest/controller/RespuestaController.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/controller/RespuestaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ import com.prosubject.prosubject.backend.apirest.service.RespuestaService;
 
 @RestController
 @RequestMapping("/api/respuestas")
-
+@CrossOrigin(origins = {"http://localhost:4200", "https://prosubject.herokuapp.com"})
 public class RespuestaController{
 	
 	@Autowired
