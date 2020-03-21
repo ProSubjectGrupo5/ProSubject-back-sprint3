ebec3e5acb3f02e76649fc410ece56fb6c8ffcbb
antromcac <antromcac@alum.us.es>
2020-03-21 17:30:54 +0100

    Errata

--- src/main/java/com/prosubject/prosubject/backend/apirest/controller/RespuestaController.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/controller/RespuestaController.java
@@ -86,7 +86,7 @@ public class RespuestaController{
 		}
 		
 		if(respuesta == null) {
-			response.put("mensaje",	 "No se ha encontrado ningun foro con espacioId: ".concat(foroId.toString()));
+			response.put("mensaje",	 "No se ha encontrado ninguna respuesta con foroId: ".concat(foroId.toString()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND); 
 		}
 		
