43f5996db296dbc85cb165ba9986d60ac6f08fa5
jeselirod <39128879+jeselirod@users.noreply.github.com>
2020-04-04 11:47:40 +0200

    Cambiando test de controladores de paquete

--- src/test/java/com/prosubject/prosubject/backend/apirest/controller/ProsubjectBackendApirestApplicationTestsController.java
+++ src/test/java/com/prosubject/prosubject/backend/apirest/controller/ProsubjectBackendApirestApplicationTestsController.java
@@ -1,4 +1,4 @@
-package com.prosubject.prosubject.backend.apirest;
+package com.prosubject.prosubject.backend.apirest.controller;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
